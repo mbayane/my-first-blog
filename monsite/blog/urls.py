a581b411a963cd8e4deadf7b92c6b91916548946
mbayane <mbaye@hann.com>
2016-01-15 12:24:14 +0100

    Ajout de vues qui permettent de créer et d'éditer un post de blog sur le site.

--- monsite/blog/urls.py
+++ monsite/blog/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    #url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	 url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
