d52a0f49a0c40072922db4322673a4919462c2a2
mbayane <mbaye@hann.com>
2016-01-15 11:49:28 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- monsite/blog/urls.py
+++ monsite/blog/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.shortcuts import render, get_object_or_404
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
